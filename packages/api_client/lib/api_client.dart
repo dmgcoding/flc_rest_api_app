6f3f503e0167657868d06ba62197fdfee7178351
shehan <dmgcoding@gmail.com>
2024-03-24 13:37:29 +0530

    feat: all repos

---
+++ packages/api_client/lib/api_client.dart
@@ -0,0 +1,8 @@
+library api_client;
+
+//all the rest api requests goes through this client
+//all of the paths are in here
+
+export 'src/api_client.dart';
+export 'src/constants/api_paths.dart';
+export 'src/models/data_state.dart';
